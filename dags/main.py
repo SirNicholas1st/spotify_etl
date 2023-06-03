154240ab94fb140b4bac679b3a030d939d515372
Tino B <bomantino@gmail.com>
2023-06-03 14:40:10 +0300

    first task of the DAG done

--- dags/main.py
+++ dags/main.py
@@ -8,10 +8,7 @@ import requests
 from urllib.parse import urlencode
 import base64
 import pandas as pd
-from flask import Flask, request
-from secrets import clientID, secret, spotify_username, spotify_password
-from threading import Thread
-from werkzeug.serving import make_server
+from api_codes import clientID, secret, spotify_username, spotify_password
 import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -35,55 +32,47 @@ default_args = {
 )
 def pipeline():
 
-    app = Flask(__name__)
-    # setting global variables
-    server = None
-    code = None
-    browser = None
-    token = None
-
-    @app.route('/callback')
-    def callback():
-        # flask callback app to receive the code.
-        global code
-        code = request.args.get('code')
-        return code
-
-    @task
-    def start_server():
-        # starting a server to host the flask app. 
-        global server
-        server = make_server("localhost", 8080, app)
-        thread = Thread(target=server.serve_forever)
-        thread.start()
-
     @task
     def launch_browser_to_get_code():
-        # Launches a browser window with arguments for the spotify api, authorizes the app by signing in with Spotify username and password defined in Api_codes.
-        # This code is required for requesting a token for the application whis is valid for 1 hour. The output code from this is one time use only. So every run requires a new one
+
         auth_headers = {
         "client_id": clientID,
         "response_type": "code",
         # redirect has to be set to the same as the one in the spotify app settings.
-        "redirect_uri": "http://localhost:8080/callback",
+        "redirect_uri": "http://localhost:7032/callback",
         "scope": "user-read-recently-played"
-    }
-        
+        }
+
         url = "https://accounts.spotify.com/authorize?" + urlencode(auth_headers)
-        global browser
-        browser = webdriver.Chrome()
-        browser.get(url)
 
-        username_input = WebDriverWait(browser, 10).until(
+        options = webdriver.ChromeOptions()
+        options.add_argument('-headless')
+        remote_webdriver = 'remote_chromedriver'
+        with webdriver.Remote(f'{remote_webdriver}:4444/wd/hub', options=options) as driver:
+            driver.get(url)
+            username_input = WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.ID, "login-username"))
-        )
+            )
 
-        password_input = WebDriverWait(browser, 10).until(
-            EC.presence_of_element_located((By.ID, "login-password"))
-        )
+            password_input = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.ID, "login-password"))
+            )
 
-        username_input.send_keys(spotify_username)
-        password_input.send_keys(spotify_password)
-        password_input.send_keys(Keys.RETURN)
+            username_input.send_keys(spotify_username)
+            password_input.send_keys(spotify_password)
+            password_input.send_keys(Keys.RETURN)
+
+            WebDriverWait(driver, 10).until(
+                EC.url_contains("localhost:7032/callback")
+            )
+            
+            code = driver.current_url.split("code=")[1]
+            return code
+
+
+    task1 = launch_browser_to_get_code()
+
+
+pipeline()
         
     
