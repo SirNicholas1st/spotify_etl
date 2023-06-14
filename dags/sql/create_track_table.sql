2d583e691be1d9b890acdbb8be0f9f2bc9f832ff
Tino B <bomantino@gmail.com>
2023-06-14 17:25:57 +0300

    changed the played at datatype to timestamp, instead of date

--- dags/sql/create_track_table.sql
+++ dags/sql/create_track_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS track_table (
     play_id int IDENTITY(1, 1),
     album_id int, 
-    played_at DATE,
+    played_at TIMESTAMP(6),
     track varchar(255),
     track_len_s int,
     FOREIGN KEY (album_id) REFERENCES album_table(album_id)
